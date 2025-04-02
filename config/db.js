ae20c253eee260f322cf15f91f95f8e78a6f5f58
niteesh577 <95271997+niteesh577@users.noreply.github.com>
2025-04-02 16:23:53 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
 
+
+// config....
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
